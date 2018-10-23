51b57ba92f786e36f85d79aee326f74f91f7ddde
Thien Nguyen <tatthien.contact@gmail.com>
2018-10-23 17:05:57 +0700

    Add dark theme (#35)
    * Add dark theme
    
    * Update javascript
    
    * Remove console.log

--- themes/til/assets/js/app.js
+++ themes/til/assets/js/app.js
@@ -92,3 +92,27 @@ searchBox.addEventListener('focus', hideResultPanel)
 searchBox.addEventListener('input', (e) => {
   searchBox.value.trim() === '' ? hideResultPanel() : showResultPanel()
 })
+
+// Switch Theme
+const switchThemeItem = document.querySelectorAll('.switch-theme-item')
+const body = document.querySelector('body')
+const themes = ['light', 'dark']
+
+let theme = localStorage.getItem('theme')
+if (themes.indexOf(theme) === -1) {
+  theme = 'light'
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  body.className = theme
+})
+
+switchThemeItem.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    let target = e.currentTarget
+    let theme = target.dataset.theme
+    localStorage.setItem('theme', theme)
+    body.className = theme
+  })
+})
