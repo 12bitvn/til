84f7569943fc194481225bfc1e2cb732bb983b4a
Thien Nguyen <tatthien.contact@gmail.com>
2020-08-22 14:24:45 +0700

    refactor(theme): remove switch theme script

--- themes/til/assets/js/app.js
+++ themes/til/assets/js/app.js
@@ -4,22 +4,3 @@ WebFont.load({
     families: ['Roboto+Mono:400,700']
   }
 })
-
-// Switch Theme
-const body = document.querySelector('body')
-const themes = ['light', 'dark']
-
-let theme = localStorage.getItem('theme')
-if (themes.indexOf(theme) === -1) {
-  theme = 'light'
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  body.className = theme
-  document.getElementById('switch-theme').addEventListener('click', (e) => {
-    e.preventDefault()
-     theme = theme === 'dark' ? 'light' : 'dark';
-    localStorage.setItem('theme', theme)
-    body.className = theme
-  })
-})
